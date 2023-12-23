b8e6cd068b895c6b837d0437224539f0323c2f8b
ReidGibson-Bingham <jsr3b0rn@gmail.com>
2023-12-22 16:12:12 -0800

    create a basic server.

--- index.js
+++ index.js
@@ -1,44 +1,17 @@
-const defaultHeight = 600;
-const defaultWidth = 800;
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const warehouses = require('./routes/warehouseRoutes');
+const inventories = require('./routes/inventoryRoutes');
 
-const puppeteer = require('puppeteer');
+const PORT = process.env.PORT || 5050;
 
-const misterFreedomSearch = (queryValue) => {
-    return `https://misterfreedom.com/search?q=${queryValue}}&type=product&options%5Bprefix%5D=last`;
-}
+app.use(cors());
+app.use(express.json());
+app.use('/', warehouses);
+app.use('/', inventories);
 
-const cart = [];
-
-(async () => {
-
-    const browser = await puppeteer.launch({ headless: 'new' });
-    const page = await browser.newPage();
-
-    // await page.goto(misterFreedomSearch('shirt'));
-    await page.goto(misterFreedomSearch('shirt'), { waitUntil: 'domcontentloaded' });
-    await page.setViewport({ width: 1280, height: 800 });
-
-    // Wait for the product elements to be available
-    await page.waitForSelector('.ProductList');
-
-    // Get all product names
-    const products = await page.$$eval('.ProductItem', (elements) => {
-        return elements.map((element) => {
-            const name = element.querySelector('.ProductItem__Title').textContent.trim();
-            const price = element.querySelector('.ProductItem__Price').textContent.trim();
-            const image = JSON.stringify(element.querySelector('.ProductItem__Image').getAttribute('data-srcset'));
-            return { name, price, image };
-        });
-    });
-
-    cart.push(products);
-
-    console.log('Products:', cart);
-
-    // Keep the browser open for a longer time (e.g., 10 seconds)
-    await page.waitForTimeout(10000);
-
-    // Close the browser
-    await browser.close();
-
-})();
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
