2112ff8cb0872340a4f311a15a970eaebf9e1f0e
ReidGibson-Bingham <jsr3b0rn@gmail.com>
2023-12-31 21:20:16 -0800

    send seed data via get products route.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const dataPath = './../jsonData';
-import data from './../jsonData/oldBlueProducts.json';
 
 router
     .route('/api/products')
     .get((_req, res) => {
-        try {
-            
-        } catch (error) {
-            console.log("error reading from file: ", error);
-            res.status(500).send('error reading from the products get route', error);
-        }
+        res.send("testing this route.");
     })
